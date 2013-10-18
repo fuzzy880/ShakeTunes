81f92a3e688fa9009e68b05d13ccc539086cb6da
Chris <sushichris880@hotmail.com>
2013-10-17 22:43:20 -0700

    Initial Commit

---
+++ ShakeTunes/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ShakeTunes
+//
+//  Created by Chris Wong on 10/17/13.
+//  Copyright (c) 2013 Chris Wong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ShakeTunes/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ShakeTunes
+//
+//  Created by Chris Wong on 10/17/13.
+//  Copyright (c) 2013 Chris Wong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
